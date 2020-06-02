86a3b4568b7c59be840f34a22881682a6ae16016
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-02 01:31:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,15 @@
 def reverse_each_word (sentance)
-  index = 0 
-  new_sentance = []
-   
-  while index < sentance.length
-    new_sentance << sentance.split  
-    index += 1 
-  end 
+  
 
 end 
+
+
+
+
+#index = 0 
+ # new_sentance = []
+   
+  #while index < sentance.length
+   # new_sentance << sentance.split  
+    #index += 1 
+  #end 
